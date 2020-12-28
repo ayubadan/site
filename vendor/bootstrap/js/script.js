7963f9972029b279c81e8cbc8e48f02c11fd7630
Apple <apple@Apples-MacBook-Pro.local>
2020-12-28 15:51:41 +0000

    first stage done

--- vendor/bootstrap/js/script.js
+++ vendor/bootstrap/js/script.js
@@ -1,3 +1,18 @@
+
 $(function() {
-    $("#row_items").hide();
+    $('.dropdown-item').on('click', async function(event) {
+        var retailer_name = await event['target']['text'];
+        $.ajax({
+            url:`https://zeyasvsme6.execute-api.eu-west-1.amazonaws.com/v2/data?retailer=${retailer_name}`,
+            type:"GET",
+            dataType: "json",
+            success: function(res){
+                console.log(res.Items.length)
+            },
+            error: function(err){
+                console.log(err)
+            }
+        })
+    });
+
 })
